a783e7b36ddbbf642d3de3526c01f3f3c4d12ca4
Sergio <sergey.dko@gmail.com>
2021-03-26 14:17:16 +0100

    robofriends - added hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,55 +1,48 @@
-import React, { Component } from 'react';
-import { Fragment } from 'react';
-import Scroll from '../components/Scroll';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import ErrorBoundary from '../components/ErrorBoundary';
-// import { render } from '@testing-library/react';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import Scroll from '../components/Scroll'
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox'
+import ErrorBoundary from '../components/ErrorBoundary'
+import './App.css'
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchfield: ''
-        };
-    }
+const App = () => {
+    const [robots, setRobots] = useState([])
+    const [searchfield, setSearchfield] = useState('')
+    // const [count, setCount] = useState(0)
 
-    componentDidMount() {
+    useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/users")
             .then(response => response.json())
-            .then(users => this.setState({ robots: users }));
+            .then(users => setRobots(users))
+        // console.log(count)
+    }, [])
+    // }, [count]) // only run if count changes
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value)
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value }); // !!
-    }
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+    })
 
-    render() {
-        const { robots, searchfield } = this.state;
-
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        });
-
-        return !robots.length ?
-            <h1>Loading . . .</h1>
-            :
-            (
-                <>
-                    <div className="tc">
-                        <h1 className="f2">RoboFriends</h1>
-                        <SearchBox searchChange={this.onSearchChange} />
-                        <Scroll>
-                            <ErrorBoundary>
-                                <CardList robots={filteredRobots} />
-                            </ErrorBoundary>
-                        </Scroll>
-                    </div>
-                </>
-            ); 
-    }
+    return !robots.length ?
+        <h1>Loading . . .</h1>
+        :
+        (
+            <>
+                <div className="tc">
+                    <h1 className="f2">RoboFriends</h1>
+                    {/* <button onClick={() => setCount(count + 1)}>Click Me!</button> */}
+                    <SearchBox searchChange={onSearchChange} />
+                    <Scroll>
+                        <ErrorBoundary>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundary>
+                    </Scroll>
+                </div>
+            </>
+        );
 }
 
 export default App;
